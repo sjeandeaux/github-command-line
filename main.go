7d399a923d6f5d27e6c0fe3b8fcccc2a58f44b61
Stéphane Jeandeaux <stephane.jeandeaux@gmail.com>
2017-06-23 10:37:18 +0200

    log end

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"os/exec"
 	"os/user"
 	"path/filepath"
-	"sync"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
@@ -61,26 +60,23 @@ func main() {
 	tc := oauth2.NewClient(oauth2.NoContext, ts)
 
 	client := github.NewClient(tc)
-	wg := new(sync.WaitGroup)
 
 	fmt.Println("Name\tSSHURL")
 
 	var err error
 	var page int
 	for page, err = 1, nil; page != 0; {
-		page, err = listRepos(client, wg, page)
+		page, err = listRepos(client, page)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-	wg.Wait()
-
 }
 
-func listRepos(client *github.Client, wg *sync.WaitGroup, page int) (int, error) {
+func listRepos(client *github.Client, page int) (int, error) {
 	var opt *github.RepositoryListByOrgOptions
 	if page > 0 {
-		opt = &github.RepositoryListByOrgOptions{"all", github.ListOptions{Page: page}}
+		opt = &github.RepositoryListByOrgOptions{Type: "all", ListOptions: github.ListOptions{Page: page}}
 	} else {
 		opt = nil
 	}
@@ -92,14 +88,14 @@ func listRepos(client *github.Client, wg *sync.WaitGroup, page int) (int, error)
 		for _, repo := range repos {
 			fmt.Printf("%s\t%s\n", *repo.Name, *repo.SSHURL)
 			if config.clone {
-				clone(*repo.Name, *repo.SSHURL, wg)
+				clone(*repo.Name, *repo.SSHURL)
 			}
 		}
 		return response.NextPage, nil
 	}
 }
 
-func clone(name string, cloneURL string, wg *sync.WaitGroup) {
+func clone(name string, cloneURL string) {
 
 	d := filepath.Join(config.directory, name)
 	//TODO check directory exists, permission
@@ -109,14 +105,10 @@ func clone(name string, cloneURL string, wg *sync.WaitGroup) {
 	if errCmd != nil {
 		fmt.Printf("error: %v\n\n", errCmd)
 	}
-	wg.Add(1)
-	go func() {
-		err := cmd.Wait()
-		if err != nil {
-			fmt.Printf("error: %v\n\n", err)
-		}
-		wg.Done()
-		fmt.Printf("%s is done\n", cloneURL)
-	}()
+	err := cmd.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\t%s done\n", name, cloneURL)
 
 }
